dd05ccad853d785624ffc2f07ce8084b1717cee5
Valden-jta <javierdlt45@gmail.com>
2024-07-22 14:48:39 +0200

    repaso2 completo

--- mobileProject/mobile.ts
+++ mobileProject/mobile.ts
@@ -1,56 +1,70 @@
-export class Mobile 
-{
-    // atributos
-    private _name: string;
-    private _trademark: string;
-    private _model: string;
-    private _color: string;
-    private _price: number;
+export class Mobile {
+  // atributos
+  private _name: string;
+  private _trademark: string;
+  private _model: string;
+  private _color: string;
+  private _price: number;
 
-    // constructor
-    constructor(name: string, trademark: string, model: string, color: string, price: number) {
-        this._name = name;
-        this._trademark = trademark;
-        this._model = model;
-        this._color = color;
-        this._price = price;
-    }
+  // constructor
+  constructor(
+    name: string,
+    trademark: string,
+    model: string,
+    color: string,
+    price: number
+  ) {
+    this._name = name;
+    this._trademark = trademark;
+    this._model = model;
+    this._color = color;
+    this._price = price;
+  }
 
-    // getters
-    public get name(): string {
-        return this._name;
-    }
-    public get trademark():string {
-        return this._trademark;
-    }
-    public get model():string {
-        return this._model;
-    }
-    public get color():string {
-        return this._color;
-    }
-    public get price():number {
-        return this._price;
-    }
+  // getters
+  public get name(): string {
+    return this._name;
+  }
+  public get trademark(): string {
+    return this._trademark;
+  }
+  public get model(): string {
+    return this._model;
+  }
+  public get color(): string {
+    return this._color;
+  }
+  public get price(): number {
+    return this._price;
+  }
 
-    // setters
-    public set name(value: string) {
-        this._name = value;
-    }
-    public set trademark(value: string) {
-        this._trademark = value;
-    }
-    public set model(value: string) {
-        this._model = value;
-    }
-    public set color(value: string) {
-        this._color = value;
-    }
-    public set price(value: number) {
-        this._price = value;
-    }
-    
-    // métodos públicos
-    // métodos privados
-    
+  // setters
+  public set name(value: string) {
+    this._name = value;
+  }
+  public set trademark(value: string) {
+    this._trademark = value;
+  }
+  public set model(value: string) {
+    this._model = value;
+  }
+  public set color(value: string) {
+    this._color = value;
+  }
+  public set price(value: number) {
+    this._price = value;
+  }
+
+  // métodos públicos
+  public print(): void {
+    console.log(`The characteristics of the mobile are:\n
+            • Name: ${this._name}\n
+            • Trademark: ${this._trademark}\n
+            • model: ${this._model}\n
+            • Color: ${this._color}\n
+            • Price: ${this._price}\n
+            `);
+  }
+
+  // métodos privados
 }
--- mobileProject/mobileTest.ts
+++ mobileProject/mobileTest.ts
@@ -1,20 +1,32 @@
 import { Mobile } from "./mobile";
 
-let movil1:Mobile = new Mobile('Redmi','Xiaomi','12 Pro','negro',124.99);
+let mobile1:Mobile = new Mobile('Redmi','Xiaomi','12 Pro','negro',124.99);
 
-console.log(movil1.name);
-console.log(movil1.trademark);
-console.log(movil1.color);
-console.log(movil1.price);
+console.log(mobile1.name);
+console.log(mobile1.trademark);
+console.log(mobile1.model);
+console.log(mobile1.color);
+console.log(mobile1.price);
 
-movil1.name = 'Galaxy';
-movil1.trademark = 'Samsung';
-movil1.color ='silver';
-movil1.price = 299.99;
+mobile1.name = 'Galaxy';
+mobile1.trademark = 'Samsung';
+mobile1.model = 'A15';
+mobile1.color ='silver';
+mobile1.price = 299.99;
 console.log('---Cambio de valores con metodos set---');
 
-console.log(movil1.name);
-console.log(movil1.trademark);
-console.log(movil1.color);
-console.log(movil1.price);
+console.log(mobile1.name);
+console.log(mobile1.trademark);
+console.log(mobile1.model);
+console.log(mobile1.color);
+console.log(mobile1.price);
 
+console.log('---Cambio de valores con metodos set---');
+mobile1.print()
+
+let mobile2:Mobile = new Mobile('Redmi','Xiaomi','12 Pro','negro',124.99);
+let mobile3:Mobile = new Mobile('Iphone','Apple','15','perla',829.95);
+
+let myMobiles:Mobile[] = [mobile1, mobile2, mobile3]
+
+myMobiles.forEach(mobile => mobile.print());
